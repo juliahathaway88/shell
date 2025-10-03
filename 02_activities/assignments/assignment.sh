24f3bd6f2ce4de9358c1848999f3a6bb479d9b9e
Julia <julia.hathaway@mail.utoronto.ca>
2025-10-03 18:39:12 -0400

    a6

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -39,16 +39,16 @@ cd data/processed
 mkdir server_logs user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cd ..
-cp raw/*server*.log server_logs/
+cp data/raw/*server*.log data/processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-cp ./*user*.log ~/newproject/data/processed/user_logs
-cp ./*event*.log ~/newproject/data/processed/event_logs
+cp data/raw/*user*.log data/processed/user_logs
+cp data/raw/*event*.log data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm ~/newproject/data/processed/user_logs/*ipaddr*
-rm ~/newproject/data/raw/*ipaddr*
+rm data/processed/user_logs/*ipaddr*
+rm data/raw/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 cd ..
-ls processed > inventory.txt
+ls data/processed > data/inventory.txt
 ###########################################
 
 echo "Project setup is complete!"
